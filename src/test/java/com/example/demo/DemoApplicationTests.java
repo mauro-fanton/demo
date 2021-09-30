df24e5dda5927e9e96e3830ddaeb86cb09378cb4
Mauro Fanton <maurofanton@ocs413-mbp.home>
2021-09-30 06:30:44 +0100

    test ciccd

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,8 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertTrue(false);
+		assertTrue(true);
+		assertFalse(true);
 	}
 
 }
